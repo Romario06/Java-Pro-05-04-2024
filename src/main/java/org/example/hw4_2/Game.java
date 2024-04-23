9398ba7d484d9cbf391d791a994889e543648234
r.chelombitko <r.chelombitko@autodoc.eu>
2024-04-23 21:32:36 +0300

    added hw4

--- src/main/java/org/example/hw4_2/Game.java
+++ src/main/java/org/example/hw4_2/Game.java
@@ -8,23 +8,23 @@ public class Game {
     private static final String[] WORDS = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
             "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper",
             "pineapple", "pumpkin", "potato"};
-    private final String guessedWord;
-    private final StringBuilder wordMask;
-    private final Scanner scanner;
+    private final String GUESSED_WORD;
+    private final StringBuilder WORD_MASK;
+    private final Scanner SCANNER;
 
     public Game() {
         Random random = new Random();
-        guessedWord = WORDS[random.nextInt(WORDS.length)];
-        wordMask = new StringBuilder("###############"); // Initial mask of 15 characters '#'
-        scanner = new Scanner(System.in);
+        GUESSED_WORD = WORDS[random.nextInt(WORDS.length)];
+        WORD_MASK = new StringBuilder("###############"); // Initial mask of 15 characters '#'
+        SCANNER = new Scanner(System.in);
     }
 
     public void start() {
         while (true) {
-            System.out.println("Guess the word: " + wordMask);
-            String guess = scanner.nextLine().toLowerCase();
+            System.out.println("Guess the word: " + WORD_MASK);
+            String guess = SCANNER.nextLine().toLowerCase();
 
-            if (guess.equals(guessedWord)) {
+            if (guess.equals(GUESSED_WORD)) {
                 System.out.println("Congratulations! You guessed the word!");
                 break; // Exit the loop if the word is guessed
             } else {
@@ -32,13 +32,13 @@ public class Game {
                 updateMask(guess);
             }
         }
-        scanner.close();
+        SCANNER.close();
     }
 
     private void updateMask(String guess) {
-        for (int i = 0; i < guessedWord.length(); i++) {
-            if (i < guess.length() && guess.charAt(i) == guessedWord.charAt(i)) {
-                wordMask.setCharAt(i, guess.charAt(i)); // Replacing the characters in the mask with the guessed ones
+        for (int i = 0; i < GUESSED_WORD.length(); i++) {
+            if (i < guess.length() && guess.charAt(i) == GUESSED_WORD.charAt(i)) {
+                WORD_MASK.setCharAt(i, guess.charAt(i)); // Replacing the characters in the mask with the guessed ones
             }
         }
     }
